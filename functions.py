3030f07e26042030158f1f0ff066599236159166
Alfred <alfred.otienoh@gmail.com>
2026-07-07 07:54:37 +0300

    Add functions.py file

---
+++ functions.py
@@ -0,0 +1,15 @@
+#Creating functions
+""" A function is defined using the key word def followed by the function name and parentheses.
+ The code block within every function starts with a colon (:) and is indented."""
+ 
+def my_function():
+    print("Hello, World!")
+    
+    # This creates a function named my_function that prints "Hello, World!" when called.
+
+# The code inside the function must be indented, and the function can be called by using its name followed by parentheses.
+
+# Calling the function
+# To call the function, simply use its name followed by parentheses:
+my_function()  # This will output: Hello, World!
+
