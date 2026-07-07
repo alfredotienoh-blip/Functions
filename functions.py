ed70964606cd601917efacd54995611a02b39b62
Alfred <alfred.otienoh@gmail.com>
2026-07-07 08:08:24 +0300

    Add callback function

--- functions.py
+++ functions.py
@@ -13,3 +13,12 @@ def my_function():
 # To call the function, simply use its name followed by parentheses:
 my_function()  # This will output: Hello, World!
 
+# You can call the same function multiple times
+my_function()  # This will output: Hello, World!
+my_function()  # This will output: Hello, World!
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5.0/9.0
+print(fahrenheit_to_celsius(32))  # This will output: 0.0
+print(fahrenheit_to_celsius(212))  # This will output: 100.0
+print(fahrenheit_to_celsius(98.6))  # This will output: 37.0
